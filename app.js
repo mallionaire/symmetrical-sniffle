6c8b3e8e6f3909a06213dc3efea1342ba3d633d0
Haz Mallion <hjmallion@gmail.com>
2022-12-14 19:24:15 +0000

    remove static serve

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const {
 const app = express();
 
 app.use(cors());
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/api', apiRouter);
